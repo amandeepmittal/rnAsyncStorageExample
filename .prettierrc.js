3d2161eb6f1706b3eadb3d78645dd4cd0b5046fe
Aman Mittal <amandeepmittal@live.com>
2020-03-03 13:18:02 +0530

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false
+}
